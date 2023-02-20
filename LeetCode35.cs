0df4ba848bad8244360d6c213ece98c65830a9b2
Justin Black <122068312+jleeblack@users.noreply.github.com>
2023-02-19 23:46:15 -0600

    Leetcode 35 - Search Insert Position
    Code Results: Top 84.2% Runtime Speed (85ms) / Top 62.41% Memory Usage (39.2mb)
    ------------------------------------------------------------------------------------------------
    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
    You must write an algorithm with O(log n) runtime complexity.
    
    Example 1:
    Input: nums = [1,3,5,6], target = 5
    Output: 2
    
    Example 2:
    Input: nums = [1,3,5,6], target = 2
    Output: 1
    
    Example 3:
    Input: nums = [1,3,5,6], target = 7
    Output: 4
     
    Constraints:
    1 <= nums.length <= 10^4
    -10^4 <= nums[i] <= 10^4
    nums contains distinct values sorted in ascending order.
    -10^4 <= target <= 10^4

---
+++ LeetCode35.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode35;
+using System;
+class Program
+{
+    static void Main(string[] args){
+        int[] nums = {1, 3, 5, 6};
+        int target = 2;
+        Console.WriteLine($"The index of the target {target} is {SearchInsert(nums, target)}");
+    }
+    public static int SearchInsert(int[] nums, int target) {
+        int dexBeg = 0; 
+        int dexEnd = nums.Length - 1;
+        while (dexBeg <= dexEnd){
+            int dexMid = dexBeg + (dexEnd - dexBeg)/2;
+            if (nums[dexMid]==target){
+                return dexMid;
+            }
+            else if (nums[dexMid]<target){
+                dexBeg = dexMid + 1;
+            }
+            else{
+                dexEnd = dexMid - 1;
+            }
+        }
+        return dexBeg;
+    }
+}
